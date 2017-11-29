6f91318e62ddfb959b563c0529e9f54f429726c4
Vinícius Chan <viniciuschan@hotmail.com>
2017-11-29 04:10:40 -0200

    Implementacao adicao

--- src/js/calculadora.js
+++ src/js/calculadora.js
@@ -1,12 +1,12 @@
 var Calculadora = {
     adicionar: function(num1, num2){
-        
         if(isNaN(num1) || isNaN(num2)){
             return 0;
         }
 
         num1 = parseFloat(num1);
         num2 = parseFloat(num2);
+        
         return num1+num2;
     },
 
